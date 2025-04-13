bcbbb6d9067236c41c216f2b5041fa0801291152
Samuel Pollak <smpollak19@gmail.com>
2025-04-13 18:06:14 -0400

    Add files via upload

---
+++ api/backend/analyst_routes.py
@@ -0,0 +1,104 @@
+########################################################
+# Sample customers blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+#------------------------------------------------------------
+# Create a new Blueprint object, which is a collection of 
+# routes.
+analysts = Blueprint('analysts', __name__)
+
+
+#------------------------------------------------------------
+# Get all customers from the system
+@analysts.route('/recipes', methods=['GET'])
+def get_recipes():
+
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT I.IngredientName, Count(S.SearchID) as PastWeekSearches
+FROM Ingredients I
+NATURAL JOIN SearchIngredient SI
+NATURAL JOIN Search S
+WHERE S.SearchDate > NOW() - INTERVAL 7 DAY;
+GROUP BY I.IngredientID
+ORDER BY COUNT(S.SearchID) DESC
+
+    ''')
+    
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+#------------------------------------------------------------
+# Get list of most popular dietary restrictions by search
+@analysts.route('/search/dietrestrict', methods=['GET'])
+def get_searches():
+    current_app.logger.info('GET /search/dietrestrict route')
+    search_info = request.json
+    days = search_info['days']
+
+    query = f'''SELECT D.RestName, Count(S.SearchID) as PastWeekSearches
+FROM DietaryRestrictions D
+NATURAL JOIN SearchDiet SI
+NATURAL JOIN Search S
+WHERE S.SearchDate > NOW() - INTERVAL {days} DAY
+GROUP BY D.DietRestID
+ORDER BY COUNT(S.SearchID) DESC
+'''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+#------------------------------------------------------------
+# Get top searched ingredients in the past x days
+@analysts.route('/search/ingredients', methods=['GET'])
+def get_searches():
+    current_app.logger.info('GET /search/ingredients route')
+    search_info = request.json
+    days = search_info['days']
+
+    query = f'''SELECT I.IngredientName, Count(S.SearchID) as PastWeekSearches
+FROM Ingredients I
+NATURAL JOIN SearchIngredient SI
+NATURAL JOIN Search S
+WHERE S.SearchDate > NOW() - INTERVAL {days} DAY;
+GROUP BY I.IngredientID
+ORDER BY COUNT(S.SearchID) DESC
+
+'''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
+
+#------------------------------------------------------------
+# Makes use of the very simple ML model in to predict a value
+# and returns it to the user
+@analysts.route('/prediction/<var01>/<var02>', methods=['GET'])
+def predict_value(var01, var02):
+    current_app.logger.info(f'var01 = {var01}')
+    current_app.logger.info(f'var02 = {var02}')
+
+    returnVal = predict(var01, var02)
+    return_dict = {'result': returnVal}
+
+    the_response = make_response(jsonify(return_dict))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
