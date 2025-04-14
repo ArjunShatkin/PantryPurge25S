42330e91fb31036cd1f448106e4ec0aa4944aa57
Cbowman15 <bowman.c@northeastern.edu>
2025-04-14 13:25:19 -0400

    changed MySQL logic for search recipe; slight UI changes

--- app/src/pages/search_recipes.py
+++ app/src/pages/search_recipes.py
@@ -6,7 +6,6 @@ from modules.nav import SideBarLinks
 SideBarLinks()
 st.title("Search for Recipes")
 st.write("Enter the ingredients you have on hand (comma-separated):")
-st.write("You may also filter recipes (all fields must have a value)")
 
 columns = st.columns(2)
 
@@ -15,8 +14,8 @@ ingredients_input = ingredients_column.text_input("Ingredients", "chicken, rice,
 
 filter_column = columns[1]
 prep_time_max = filter_column.number_input("Max Preparation Time (minutes)", min_value=0, value=360)
-cuisine = filter_column.text_input("Cuisine", "Italian")
-diet_rest = filter_column.text_input("Diet", "Vegetarian")
+cuisine = filter_column.text_input("Cuisine", "")
+diet_rest = filter_column.text_input("Diet", "")
 
 if st.button("Search"):
     ingredients_results = []
@@ -30,23 +29,23 @@ if st.button("Search"):
         if ingredients_response.status_code == 200:
             ingredients_results = ingredients_response.json()
 
-    used_filter = prep_time_max > 0 and cuisine.strip() and diet_rest.strip()
+    used_filters = {}
+    if prep_time_max > 0:
+        used_filters["prep_time_max"] = prep_time_max
+    if cuisine:
+        used_filters["cuisine"] = cuisine.strip()
+    if diet_rest:
+        used_filters["diet_rest"] = diet_rest.strip()
 
-    if used_filter:
-        filters = {
-            "prep_time_max": prep_time_max,
-            "cuisine": cuisine.strip(),
-            "diet_rest": diet_rest.strip()
-        }
+    if used_filters:
         filter_url = "http://api:4000/casual_cooks/recipes/filter"
-        filter_response = requests.get(filter_url, params=filters)
+        filter_response = requests.get(filter_url, params=used_filters)
         if filter_response.status_code == 200:
             filter_results = filter_response.json()
 
-    search_results = []
-    if used_filter:
-        recipe_ids = [recipe['RecipeID'] for recipe in filter_results]
-        search_results = [recipe for recipe in ingredients_results if recipe['RecipeID'] in recipe_ids]
+    if ingredients_results and not filter_results:
+        found_ids = {recipe['RecipeID'] for recipe in filter_results}
+        search_results = [recipe for recipe in ingredients_results if recipe['RecipeID'] in found_ids]
     else:
         search_results = ingredients_results
 
