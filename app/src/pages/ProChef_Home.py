390cdb7e396d9d46f03d73866a216d6740ae2663
Arjun Shatkin <arjunshatkin@Arjuns-MacBook-Pro.local>
2025-04-14 15:10:29 -0400

    Adding the ProChef_Home to pages

---
+++ app/src/pages/ProChef_Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+from modules.nav import SideBarLinks
+
+SideBarLinks()
+
+st.title("Welcome Professional Chef, Anthony")
+st.write("Welcome to the Professional Chef Home Page!")
+
+if st.button("View Your Recipes"):
+    st.switch_page("pages/search_recipes.py")
+
+if st.button("Add A Recipe"):
+    st.switch_page("pages/search_recipes.py")
+
+if st.button("Update A Recipe"):
+    st.switch_page("pages/search_recipes.py")
+
+if st.button("View Nearby Chefs"):
+    st.switch_page("pages/search_recipes.py")
+
+
+
