0fb7a15700ac2b44fd9f246ee1e1b1664f4bdffa
Arjun Shatkin <arjunshatkin@Arjuns-MacBook-Pro.local>
2025-04-14 15:15:19 -0400

    adding a view recipes pge

---
+++ app/src/pages/100_ViewRecipes.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import requests
+from modules.nav import SideBarLinks
+
+SideBarLinks()
+
+st.title("Welcome Professional Chef, Anthony")
+st.write("Welcome to the Professional Chef Home Page!")
