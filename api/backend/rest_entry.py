911a6e405a47749786b40a0729361112cba767cd
Cbowman15 <150717861+Cbowman15@users.noreply.github.com>
2025-04-14 15:24:19 -0400

    changed url prefix

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -53,7 +53,7 @@ def create_app():
     app.register_blueprint(recipes, url_prefix='/r')
     app.register_blueprint(chefs, url_prefix='/c')
     app.register_blueprint(issues, url_prefix='/i')
-    app.register_blueprint(casual_cooks, url_prefix='/cc')
+    app.register_blueprint(casual_cooks, url_prefix='/casual_cooks')
 
     # Don't forget to return the app object
     return app
