dcefcd913bd8a70e7172e85bccabc7a6d2886c4a
Cbowman15 <bowman.c@northeastern.edu>
2025-04-14 13:24:30 -0400

    switch page link; quick fix

--- app/src/pages/CC_Home.py
+++ app/src/pages/CC_Home.py
@@ -9,13 +9,5 @@ st.title("Casual Cook Home")
 st.write("Welcome to the Casual Cook Home Page!")
 
 st.header("Features")
-st.write("Search for Recipes")
-st.write("Review History")
-st.write("Recommendations")
-
 if st.button("Search for Recipes"):
-    st.switch_page("search_recipes.py")
-if st.button("Review History"):
-    st.write("Review History is not implemented yet.")
-if st.button("Recommendations"):
-    st.write("Recommendations is not implemented yet.")
+    st.switch_page("pages/search_recipes.py")
