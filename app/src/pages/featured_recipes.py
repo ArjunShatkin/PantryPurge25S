4740355b6ab97b778ffbebf21f3af48266d2290c
Cbowman15 <bowman.c@northeastern.edu>
2025-04-15 16:53:24 -0400

    writes featured recipes to page

---
+++ app/src/pages/featured_recipes.py
@@ -0,0 +1,21 @@
+
+import streamlit as st
+import requests
+from modules.nav import SideBarLinks
+
+SideBarLinks()
+st.title("Featured Recipes")
+
+featured_url = "http://web-api-test:4000/cc/recipes/featured"
+response = requests.get(featured_url)
+
+if response.status_code == 200:
+    recipes = response.json()
+    if recipes:
+        for recipe in recipes:
+            st.write("\n\n")
+            st.write(f" {recipe.get('RecipeName')}")
+            st.write(recipe.get('Description'))
+            if st.button("View Recipe", key=f"view_{recipe['RecipeID']}"):
+                st.session_state["selected_id"] = recipe['RecipeID']
+                st.switch_page("recipe_profile.py")
