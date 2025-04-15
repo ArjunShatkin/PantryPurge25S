0a2398df6a39ee0ce3ed5df309ba1cfc86e4b488
Cbowman15 <bowman.c@northeastern.edu>
2025-04-15 19:45:04 -0400

    delete template imports

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
 from backend.db_connection import db
-from backend.customers.customer_routes import customers
-from backend.products.products_routes import products
-from backend.simple.simple_routes import simple_routes
 from backend.simple.user_routes import users
 from backend.simple.arjundemo import recipes
 from backend.simple.arjundemo import chefs
@@ -47,11 +44,8 @@ def create_app():
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
-    app.register_blueprint(simple_routes)
     app.register_blueprint(users, url_prefix ='/u')
     app.register_blueprint(issues, url_prefix ='/i')
-    app.register_blueprint(customers,   url_prefix='/customers')
-    app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(recipes, url_prefix='/r')
     app.register_blueprint(analysts, url_prefix='/a')
     app.register_blueprint(casual_cooks, url_prefix='/cc')
